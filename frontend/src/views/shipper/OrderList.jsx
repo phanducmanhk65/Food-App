db2b30d5543848b5c93204f249c107841dcdb167
nguyenlong266 <nhhlong.ct@gmail.com>
2023-07-25 13:41:56 +0700

    shipper page

--- frontend/src/views/shipper/OrderList.jsx
+++ frontend/src/views/shipper/OrderList.jsx
@@ -3,9 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const OrderList = () => {
   const orders = [
-    { id: 1, customer: 'Khách hàng 1', address: 'Địa chỉ 1' },
-    { id: 2, customer: 'Khách hàng 2', address: 'Địa chỉ 2' },
-    { id: 3, customer: 'Khách hàng 3', address: 'Địa chỉ 3' },
+    { id: 1, customer: 'Khách hàng 1', phone: '0123456789', address: 'Địa chỉ 1', price: 100 },
+    { id: 2, customer: 'Khách hàng 2', phone: '0987654321', address: 'Địa chỉ 2', price: 150 },
+    { id: 3, customer: 'Khách hàng 3', phone: '0369845621', address: 'Địa chỉ 3', price: 200 },
+    // Thêm các đơn hàng khác tùy ý
   ];
 
   const handleAcceptOrder = (orderId) => {
@@ -15,28 +16,21 @@ const OrderList = () => {
   return (
     <div>
       <h2>Danh sách đơn:</h2>
-      <table className="table table-striped"> 
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Khách hàng</th>
-            <th>Địa chỉ</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map((order) => (
-            <tr key={order.id}>
-              <td>{order.id}</td>
-              <td>{order.customer}</td>
-              <td>{order.address}</td>
-              <td>
+      <div className="row">
+        {orders.map((order) => (
+          <div key={order.id} className="col-lg-2 mb-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{order.customer}</h5>
+                <p className="card-text">Số điện thoại: {order.phone}</p>
+                <p className="card-text">Địa chỉ: {order.address}</p>
+                <p className="card-text">Giá đơn: ${order.price}</p>
                 <button className="btn btn-primary" onClick={() => handleAcceptOrder(order.id)}>Nhận đơn</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
