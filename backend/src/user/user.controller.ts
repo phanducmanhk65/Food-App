02d2f8fe045a303b1dd6ef1d6da7d75f34eeb7bc
nguyenlong266 <nhhlong.ct@gmail.com>
2023-07-27 09:14:54 +0700

    update note

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
