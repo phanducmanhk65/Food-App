ad345d6fc8cf8469cef3a01faa7f440c7f50c9bd
tunld23 <buicongtuan1478@gmail.com>
2023-08-01 17:23:39 +0700

    API Login + Register Yayyyy

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -1,17 +1,15 @@
-import React, { useContext } from "react";
-import ShopContext from "../../context/shop-context";
+import React from "react";
+
 import { Products } from "../../constants";
 import CartItem from "./CartItem";
 
 const Cart = () => {
-  const { cartItems } = useContext(ShopContext);
-
   return (
     <div className="cart">
       <div>
         <h1>Your Cart Items</h1>
       </div>
-      <div className="cart">
+      {/* <div className="cart">
         {Products.map((product) => {
           if (cartItems[product.id] !== 0) {
             return <CartItem data={product} />;
@@ -19,7 +17,7 @@ const Cart = () => {
           // Nếu số lượng sản phẩm là 0 thì không cần trả về gì
           return null;
         })}
-      </div>
+      </div> */}
     </div>
   );
 };
