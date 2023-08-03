14fff7ab85171bc5604fb3f9f666e1f3f97e8469
nguyenlong266 <nhhlong.ct@gmail.com>
2023-08-03 16:56:42 +0700

    commit

--- backend/src/restaurant/restaurant.controller.ts
+++ backend/src/restaurant/restaurant.controller.ts
@@ -1,40 +1,76 @@
-import { Controller, Get, Post, Body,Put,Patch, Delete, Param, UseGuards, Request } from "@nestjs/common";
-import { RestaurantService } from "./restaurant.service";
-import { CreateRestaurantDto } from "./dto/create-restaurant.dto";
-import { UpdateRestaurantDto } from "./dto/update-user.dto";
-import { Restaurant } from "./entities/restaurant.entity";
-import { Goard } from "../middleware/goard"; 
+/* eslint-disable prettier/prettier */
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Patch,
+  Delete,
+  Param,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
+import { RestaurantService } from './restaurant.service';
+import { CreateRestaurantDto } from './dto/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dto/update-user.dto';
+import { Restaurant } from './entities/restaurant.entity';
+import { Goard } from '../middleware/goard';
 
 @Controller('restaurant')
 export class RestaurantController {
-    constructor(private readonly restaurantService: RestaurantService){};
+  constructor(private readonly restaurantService: RestaurantService) {}
 
-    @Post('/create')
-    @UseGuards(Goard)
-    create(@Body() restaurant: {name: string, address: string, phoneNumber: string, latitude: number, longitude: number}
-    , @Request() req) {
-        const data = new CreateRestaurantDto(req.idUser, restaurant.name, restaurant.address, restaurant.phoneNumber, restaurant.latitude, restaurant.longitude);
-        return this.restaurantService.create(data);
+  @Post('/create')
+  @UseGuards(Goard)
+  create(
+    @Body()
+    restaurant: {
+      name: string;
+      address: string;
+      phoneNumber: string;
+      latitude: number;
+      longitude: number;
+    },
+    @Request() req,
+  ) {
+    const data = new CreateRestaurantDto(
+      req.idUser,
+      restaurant.name,
+      restaurant.address,
+      restaurant.phoneNumber,
+      restaurant.latitude,
+      restaurant.longitude,
+    );
+    return this.restaurantService.create(data);
+  }
 
-    }
+  @Get('/myrestaurant')
+  @UseGuards(Goard)
+  findMyRestaurant(@Request() req) {
+    return this.restaurantService.findMyRestaurant(req.idUser);
+  }
 
-    @Get('/myrestaurant')
-    @UseGuards(Goard)
-    findMyRestaurant(@Request() req) {
-        return this.restaurantService.findMyRestaurant(req.idUser);
+  @Put('/updateres/:idR')
+  @UseGuards(Goard)
+  update(
+    @Param('idR') idR: number,
+    @Request() req,
+    @Body()
+    updateRes: {
+      name: string;
+      address: string;
+      phoneNumber: string;
+      latitude: number;
+      longitude: number;
+    },
+  ) {
+    return this.restaurantService.update(req.idUser, idR, updateRes);
+  }
 
-    }
-
-    @Put('/updateres/:idR')
-    @UseGuards(Goard)
-    update(@Param('idR') idR: number,@Request() req, @Body() updateRes: {name: string, address: string, phoneNumber: string, latitude: number, longitude: number}) {
-        return this.restaurantService.update(req.idUser,idR ,updateRes )
-    }
-
-    @Delete('/deleteres/:idR')
-    @UseGuards(Goard)
-    remove(@Param('idR') id:number, @Request() req) {
-         return this.restaurantService.remove(req.idUser,+id);
-         
-}
+  @Delete('/deleteres/:idR')
+  @UseGuards(Goard)
+  remove(@Param('idR') id: number, @Request() req) {
+    return this.restaurantService.remove(req.idUser, +id);
+  }
 }
