73eab028cea1a22bd498f972f05f07c8836b1582
nguyenlong266 <nhhlong.ct@gmail.com>
2023-08-07 15:18:07 +0700

    commit

--- frontend/src/views/Test.js
+++ frontend/src/views/Test.js
@@ -1,25 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+
+
 const OrdersWaiting = () => {
   const [orders, setOrders] = useState([]);
-  const [idUser, setIdUser] = useState(null);
   const [error, setError] = useState(null);
 
-  const fetchUserId = async (id) => {
-    try {
-      const response = await axios.get(`http://localhost:3000/user`);
-      setIdUser(response.data.id);
-    } catch (error) {
-      console.error("Error fetching user ID:", error);
-      setError(error);
-    }
-  };
 
   const fetchOrders = async () => {
-    if (!idUser) return;
     try {
-      const response = await axios.get(`http://localhost:3000/order/findOrdership/${idUser}`);
+      const response = await axios.get(`http://localhost:3000/order/findOrdership/1`,
+        {
+          withCredentials: true
+        }
+      );
       setOrders(response.data);
     } catch (error) {
       console.error("Error fetching orders:", error);
@@ -30,7 +25,10 @@ const OrdersWaiting = () => {
   const handleAcceptOrder = async (order) => {
     try {
       const updatedOrder = { ...order, status: 2 };
-      await axios.put(`http://localhost:3000/order/updateorder`, updatedOrder);
+      await axios.put(`http://localhost:3000/order/updateorder`, updatedOrder,
+        {
+          withCredentials: true
+        });
       setOrders(orders.map(o => o.id === order.id ? updatedOrder : o));
     } catch (error) {
       console.error("Error updating order:", error);
@@ -38,15 +36,10 @@ const OrdersWaiting = () => {
     }
   };
 
-  useEffect(() => {
-    fetchUserId();
-  }, []);
 
   useEffect(() => {
-    if (idUser) {
-      fetchOrders();
-    }
-  }, [idUser]);
+    fetchOrders();
+  }, []);
 
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -59,10 +52,10 @@ const OrdersWaiting = () => {
           <div className="card">
             <div className="card-body">
               <h5 className="card-title">{order.shop}</h5>
-              <p className="card-text">Địa chỉ quán: {order.shopAddress}</p>
-              <p className="card-text">Tên khách hàng: {order.customer}</p>
-              <p className="card-text">Số điện thoại: {order.phone}</p>
-              <p className="card-text">Giá đơn: ${order.price}</p>
+              <p className="card-text">Địa chỉ quán: {order.idRestaurant}</p>
+              <p className="card-text">Tên khách hàng: {order.idCustomer}</p>
+              <p className="card-text">Note: {order.note}</p>
+              <p className="card-text">Giá đơn: ${order.totalPrice}</p>
               <button className="btn btn-primary" onClick={() => handleAcceptOrder(order)}>Nhận đơn</button>
             </div>
           </div>
