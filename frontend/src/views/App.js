8edc684236aeb2b0843d3f808e8dd176330cd995
nguyenlong266 <nhhlong.ct@gmail.com>
2023-08-09 11:22:22 +0700

    commit

--- frontend/src/views/App.js
+++ frontend/src/views/App.js
@@ -1,12 +1,10 @@
 import "./App.scss";
-import AdminPanel from "./administration/AdminPanel";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./Home/HomePage";
+import "bootstrap/dist/css/bootstrap.min.css"; import AdminPanel from "./administration/AdminPanel";
 import ShipperPage from "./shipper/ShipperPage";
 import Test from "./Test";
-import { Register } from "../components";
-import OrdersWaiting from "./shipper/OrdersWaiting";
-
+// import { Register } from "../components";
+// import OrdersWaiting from "./shipper/OrdersWaiting";
+// import Home from "./Home/HomePage";
 // import ShopContextProvider from "../context/shop-context";
 
 function App() {
