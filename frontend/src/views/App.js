3fa30400b3332452fb39de84e0f23f624d891fdf
nguyenlong266 <nhhlong.ct@gmail.com>
2023-08-16 14:33:20 +0700

    commit

--- frontend/src/views/App.js
+++ frontend/src/views/App.js
@@ -4,12 +4,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // import Home from "./Home/HomePage";
 // import { Provider } from "react-redux";
 // import store from "../store/stores";
-import AdminPanel from "./administration/AdminPanel";
+import AdminPanel from "./administration/AdminPanel"
 import ShipperPage from "./shipper/ShipperPage"
 
 function App() {
   return (
-    <ShipperPage />
+    <AdminPanel />
   );
 }
 export default App;
