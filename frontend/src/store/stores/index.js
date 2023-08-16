7b18190e36c3ccdc962465ed8977db3ff3e695de
nguyenlong266 <nhhlong.ct@gmail.com>
2023-08-16 11:23:57 +0700

    commit

--- frontend/src/store/stores/index.js
+++
@@ -1,5 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import rootReducer from '../reducers/index'
-const store =  createStore(rootReducer,applyMiddleware(thunkMiddleware));
-export default store;
